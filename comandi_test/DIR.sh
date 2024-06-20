14f4c799238c56ad8deb86151dd7653e4c99eb3e
Matteo Malvezzi <matteomalvezzi05@gmail.com>
2024-06-20 18:21:24 +0200

    remove test folder

--- comandi_test/DIR.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo SONO DIR.sh
-echo 'Il valore di $0 ===>' $0
-echo 'Il valore di $1 ===>' $1
-echo "DEBUG-Ora eseguo ls -l $1"
-ls -l $1
--- comandi_test/prova-comandi.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo "path assoluto di p.txt : "
-pwd p.txt
-echo "directory corrente: "
-pwd
